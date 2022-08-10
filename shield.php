9e7ca20e18cbddf76c85c51b2b90079f3ceebbaf
Kanvret12 <82053023+Kanvret12@users.noreply.github.com>
2022-08-10 19:42:13 +0700

    Add files via upload

---
+++ shield.php
@@ -0,0 +1,11 @@
+<?php
+	$host = gethostname(); 
+?>
+<html>
+<head>
+	<h1> WEBSITE <?php echo $host ; ?> </h1>
+</head>
+<body>
+	<h1> WELCOME TO MY WEBSITE <?php echo $host ; ?></h1>
+</body> 
+</html>
